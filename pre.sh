e5981d6c232210f14bc79d2fa8ac85782f3e88f2
johnkwilliam10 <34278295+johnkwilliam10@users.noreply.github.com>
2024-01-21 20:54:38 +0300

    Update pre.sh

--- pre.sh
+++ pre.sh
@@ -1,1 +1,26 @@
 #!/bin/sh
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+echo -e "${GREEN}Downloading licensing System${NC}"
+wget -O https://raw.githubusercontent.com/johnkwilliam10/kalawa/main/kalawacpanel > /dev/null 2>&1
+chmod +x /bin/kalawacpanel
+
+sleep 2
+if [ ! -f /bin/kalawacpanel ]
+then
+wget -O /usr/bin/kalawacpanel https://raw.githubusercontent.com/johnkwilliam10/kalawa/main/kalawacpanel > /dev/null 2>&1
+chmod +x /bin/kalawacpanel
+fi
+
+if [ "$1" == "install" ]; then
+  /bin/kalawacpanel $1
+else
+  echo "{RED}Add install option${NC}"
+fi
+
+rm -rf pre.sh
+
+exit 0
