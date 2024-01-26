8168d16cc64974ffa17309a5e037e814128a2176
johnkwilliam10 <34278295+johnkwilliam10@users.noreply.github.com>
2024-01-26 19:33:41 +0300

    Update pre.sh

--- pre.sh
+++ pre.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 RED='\033[1;91m'
-GREEN='\033[1;92m'
 NC='\033[0m'
-
-echo -e "${GREEN} Starting licensing System..........${NC}"
 
 if [ "$1" == "cpanel" ]; then
   wget -O /usr/bin/kalawa$1 https://raw.githubusercontent.com/johnkwilliam10/kalawa/main/kalawa$1 > /dev/null 2>&1
